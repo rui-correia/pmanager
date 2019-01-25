d7f178dba311c4a68a73d14258663ba33f3f13a2
Rui Correia <rui.braz.correia@gmail.com>
2019-01-24 21:51:11 -0300

    commit terminando aula 12

--- database/migrations/2019_01_24_234627_create_comments_table.php
+++ database/migrations/2019_01_24_234627_create_comments_table.php
@@ -21,7 +21,7 @@ class CreateCommentsTable extends Migration
             $table->integer('commentable_id')->unsigned();
 
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users')
+            $table->foreign('user_id')->references('id')->on('users');
 
             $table->timestamps();
         });
